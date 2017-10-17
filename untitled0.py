0d693ce75fb24d4d984396c75b9c1c7b8ec979f4
JONG086 <32867926+JONG086@users.noreply.github.com>
2017-10-17 18:21:04 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 17:19:02 2017
+
+@author: SPMS2017
+"""
+
+r = (int(input('Please enter the rows:')))
+
+while r <= 0: 
+    print('error')
+    r = (int(input('Please enter the rows:')))
+
+c = (int(input('Please enter the columns:')))
+
+while c <= 0:
+    print('error')
+    c = (int(input('Please enter the columns:')))
+
+import numpy as np
+
+my_mat = np.zeros([r,c])
+
+print(my_mat)
+
+player = int(input('Do you want to be player 1 or player 2? (please input either 1 or 2)'))
+
+while player != 1 and player !=2:
+    print('error')
+    player = int(input('Do you want to be player 1 or player 2? (please input either 1 or 2)'))
+
